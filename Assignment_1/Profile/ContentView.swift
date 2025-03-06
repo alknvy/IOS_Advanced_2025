2047b1faabb71cb7a7985f331d091e00a3370893
alknvy <145662449+alknvy@users.noreply.github.com>
2025-03-07 01:12:03 +0500

    Add files via upload

---
+++ Assignment_1/Profile/ContentView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct MainProfileView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image("profile_picture") // Добавьте фото в Assets.xcassets
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+
+                Text("Yersultan Alkenov")
+                    .font(.title)
+                    .bold()
+
+                Text("19 years old | Almaty")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+
+                Text("iOS Developer learning SwiftUI")
+                    .multilineTextAlignment(.center)
+                    .padding()
+
+                Spacer()
+
+                NavigationLink("My Hobbies", destination: InterestsView())
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+
+                NavigationLink("My Goals", destination: GoalsView())
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+
+                Spacer()
+            }
+            .navigationTitle("My Profile")
+        }
+    }
+}
+
+#Preview {
+    MainProfileView()
+}
